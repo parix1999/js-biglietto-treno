7b936f0feb441f2fef609aaaaba6f5b1db0b2eb5
parix1999 <parisottoedoardo@gmail.com>
2021-05-28 15:26:59 +0200

    Ticket-1.0

---
+++ js/script.js
@@ -0,0 +1,5 @@
+alert('Ciao sono javascript e sono al tuo servizio! Ti calcolerò il prezzo del tuo biglietto!')
+alert('Se ha meno di 18 anni hai un 20% di sconto, mentre, se ne hai più di 65 un 40% ;)');
+
+var numberKm = ('Quanti km devi fare ?');
+var age = ('Quanti anni hai ?');
