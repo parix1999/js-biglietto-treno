281a13f270927a12755b78a4deeb4e503f50b29f
parix1999 <parisottoedoardo@gmail.com>
2021-05-28 15:46:28 +0200

    ticket-3.0

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 alert('Ciao sono javascript e sono al tuo servizio! Ti calcolerò il prezzo del tuo biglietto!')
 alert('Se ha meno di 18 anni hai un 20% di sconto, mentre, se ne hai più di 65 un 40% ;)');
 
-var numberKm = ('Quanti km devi fare ?');
-var age = ('Quanti anni hai ?');
+var numberKm = prompt('Quanti km devi fare ?');
+var age = prompt('Quanti anni hai ?');
 
 var outputElement = document.getElementById('prezzo-treno');
 
-// inizio calcoli con il se
+// inizio calcoli con il if
+
+if (age >= 18 && age<65){
+    var calcAdult = numberKm * 0.21;
+    outputElement.innerHTML = (calcAdult.toFixed(2)); 
+}
